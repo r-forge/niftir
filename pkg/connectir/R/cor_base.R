f0e921534472260b2c7cb86ddc378446fba9862f
zarrar (Zarrar Shehzad) <zarrar.shehzad@yale.edu>
2011-09-04 07:44:19 +0000

    new armadillo based correlation functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/niftir@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/connectir/R/cor_base.R
+++ pkg/connectir/R/cor_base.R
@@ -19,3 +19,117 @@ vbca_batch <- function(subs.bigmats, cols, ztransform=FALSE, ...) {
     nsubjects <- length(subs.bigmats)
     lapply(1:nsubjects, function(i) vbca(subs.bigmats[[i]], cols, ztransform, ...))
 }
+
+
+
+## NEW CODE USING ARMADILLO
+
+library(RcppArmadillo)
+library(inline)
+
+plugin_bigmemory <- function() {
+  l <- getPlugin("RcppArmadillo")
+  
+  l$includes <- paste(l$includes, '
+#include "bigmemory/BigMatrix.h"
+#include "bigmemory/MatrixAccessor.hpp"
+#include "bigmemory/bigmemoryDefines.h"
+#include "bigmemory/isna.hpp"
+')
+  
+  l$LinkingTo <- c("bigmemory", l$LinkingTo)
+  
+  l$Depends <- c("bigmemory", l$Depends)  
+  
+  return(l)
+}
+registerPlugin("bigmemory", plugin_bigmemory)
+
+
+cpp_cor <- cxxfunction( signature(As="object", Bs="object", Cs="object"), 
+'
+  try{
+    using namespace Rcpp;
+    
+    SEXP addr; BigMatrix *pMat; index_type offset; double *ptr_double;
+    
+    RObject Abm(As);
+    addr = Abm.slot("address");
+    pMat = reinterpret_cast<BigMatrix*>(R_ExternalPtrAddr(addr));
+    offset = pMat->nrow() * pMat->col_offset();
+    ptr_double = reinterpret_cast<double*>(pMat->matrix()) + offset;
+    arma::mat A(ptr_double, pMat->nrow(), pMat->ncol(), false);
+    
+    RObject Bbm(Bs);
+    addr = Bbm.slot("address");
+    pMat = reinterpret_cast<BigMatrix*>(R_ExternalPtrAddr(addr));
+    offset = pMat->nrow() * pMat->col_offset();
+    ptr_double = reinterpret_cast<double*>(pMat->matrix()) + offset;
+    arma::mat B(ptr_double, pMat->nrow(), pMat->ncol(), false);
+    double df = pMat->nrow() - 1;
+    
+    RObject Cbm(Cs);
+    addr = Cbm.slot("address");
+    pMat = reinterpret_cast<BigMatrix*>(R_ExternalPtrAddr(addr));
+    offset = pMat->nrow() * pMat->col_offset();
+    ptr_double = reinterpret_cast<double*>(pMat->matrix()) + offset;
+    arma::mat C(ptr_double, pMat->nrow(), pMat->ncol(), false);
+    
+    C = (arma::trans(A) * B)/df;
+    
+    return R_NilValue;
+  } catch( std::exception &ex ) {
+  forward_exception_to_r( ex );
+  } catch(...) { 
+  ::Rf_error( "c++ exception (unknown reason)" ); 
+  }
+  return R_NilValue; // -Wall
+', plugin = "bigmemory")
+
+cpp_ztransform <- cxxfunction( signature(Cs="object"), 
+'
+  try{
+    using namespace Rcpp;
+    
+    SEXP addr; BigMatrix *pMat; index_type offset; double *ptr_double;
+    
+    RObject Cbm(Cs);
+    addr = Cbm.slot("address");
+    pMat = reinterpret_cast<BigMatrix*>(R_ExternalPtrAddr(addr));
+    offset = pMat->nrow() * pMat->col_offset();
+    ptr_double = reinterpret_cast<double*>(pMat->matrix()) + offset;
+    arma::mat C(ptr_double, pMat->nrow(), pMat->ncol(), false);
+    
+    atanh(C);
+    
+    return R_NilValue;
+  } catch( std::exception &ex ) {
+  forward_exception_to_r( ex );
+  } catch(...) { 
+  ::Rf_error( "c++ exception (unknown reason)" ); 
+  }
+  return R_NilValue; // -Wall
+', plugin = "bigmemory")
+
+
+vbca2 <- function(bigmat, cols, ztransform=FALSE, outmat=NULL, ...) {
+        A <- deepcopy(bigmat, cols=cols)
+    #    A <- sub.big.matrix(bigmat, firstCol=firstCol, lastCol=lastCol)
+    	B <- bigmat
+    	if (is.null(outmat))
+    	    C <- big.matrix(ncol(A), ncol(B), init=0, type="double", ...)
+    	else if (ncol(A)==nrow(outmat) && ncol(B)==ncol(outmat))
+    	    C <- outmat
+    	else
+    	    stop("dimensions of outmat are out of whack")
+    	
+    	cpp_cor(A, B, C)
+    	if (ztransform)
+    	    atanh(C)
+    	invisible(C)
+}
+
+vbca_batch2 <- function(subs.bigmats, cols, ztransform=FALSE, ...) {
+    nsubjects <- length(subs.bigmats)
+    lapply(subs.bigmats, function(x) vbca2(x, cols, ztransform, ...))
+}
--- pkg/connectir/R/subdist.R
+++ pkg/connectir/R/subdist.R
@@ -319,3 +319,96 @@ slice.subdist <- function(bigmat, subs=1:sqrt(nrow(bigmat)), voxs=1:ncol(bigmat)
     matinds <- as.vector(matinds[subs,subs])
     deepcopy(bigmat, cols=voxs, rows=matinds, ...)
 }
+
+
+
+
+### NEW CODE USING ARMADILLO
+
+compute_subdist2 <- function(funclist, subdist, seed_inds, blocksize, ztransform, start=1, verbose=TRUE, testonly=FALSE) {
+    nseeds <- length(seed_inds)
+    blocks <- niftir.split.indices(start, nseeds, by=blocksize)
+    
+#    dfun <- function(i, blocks, seed_inds, funclist, subdist, ztransform, verbose, pb) {
+    dfun <- function(i, ...) {
+        if (verbose) {
+            update(pb, i)
+            #msg <- sprintf("\nblock %i with voxels %i:%i\n", i, blocks$starts[i], blocks$ends[i])
+            #cat(msg)
+        }
+        inds_CHUNK <- seed_inds[blocks$starts[i]:blocks$ends[i]]
+        cormaps_list <- vbca_batch2(funclist, inds_CHUNK, ztransform=ztransform, shared=FALSE)
+        subdist_CHUNK <- sub.big.matrix(subdist, firstCol=blocks$starts[i], lastCol=blocks$ends[i])
+        tmp <- compute_subdist_worker2(cormaps_list, inds_CHUNK, subdist_CHUNK)
+        rm(inds_CHUNK, subdist_CHUNK, tmp, cormaps_list)
+        gc(FALSE)
+        return(NULL)
+    }
+    
+    # Test
+    i <- 1
+    if (verbose) {
+        cat("...running a test (", blocks$starts[i],  ")\n")
+        pb <- progressbar(i)
+    } else {
+        pb <- NULL
+    }
+    dfun(i)
+    check_dmat(matrix(subdist[,blocks$starts[i]], sqrt(nrow(subdist))))
+    check_dmat(matrix(subdist[,blocks$ends[i]], sqrt(nrow(subdist))))
+    if (verbose)
+        end(pb)
+    if (testonly) {
+        cat("...test only...\n")
+        return(NULL)
+    }
+    
+    # Subdist Calculation
+    if (verbose) {
+        cat("...now the real deal\n")
+        pb <- progressbar(blocks$n)
+    } else {
+        pb <- NULL
+    }
+    
+    if (getDoParRegistered() && getDoParWorkers() > 1) {
+        lo <- min(getDoParWorkers()*3, blocks$n-1)
+        superblocks <- niftir.split.indices(2, blocks$n, length.out=lo)
+        foreach(si=1:superblocks$n, .packages=c("connectir"), .inorder=TRUE) %dopar% 
+            for(i in superblocks$starts[si]:superblocks$ends[si]) dfun(i)
+    }
+    else {
+        for (i in 2:blocks$n)
+            dfun(i)
+    }
+    
+    if (verbose)
+        end(pb)
+}
+
+compute_subdist_worker2 <- function(sub.cormaps, inds, outmat=NULL, type="double", ...) {
+    nsubs <- length(sub.cormaps)
+    nvoxs <- ncol(sub.cormaps[[1]])
+    nseeds <- nrow(sub.cormaps[[1]])
+    if (nseeds != length(inds))
+        stop("length of inds doesn't match nrow of first sub.cormaps element")
+    
+    if (is.null(outmat))
+        outmat <- big.matrix(nsubs^2, nseeds, type=type, shared=TRUE, ...)
+    else if (ncol(outmat) != nseeds || nrow(outmat) != nsubs^2)
+        stop("dimensions of outmat do not match nsubs and nseeds values")
+    
+    subsMap <- big.matrix(nvoxs-1, nsubs, type=type, shared=FALSE, ...)
+    voxs <- 1:nvoxs
+    for (i in 1:nseeds) {
+        .Call("CombineSubMapsMain", sub.cormaps, subsMap@address, as.double(i), as.double(voxs[-inds[i]]), as.double(nvoxs-1), as.double(nsubs))
+        col <- sub.big.matrix(outmat, firstCol=i, lastCol=i)
+        cpp_cor(subsMap, subsMap, col)
+        .Call("BigSubtractScalarMain", col@address, as.double(1), TRUE);
+    }
+    
+    rm(subsMap)
+    gc(F)
+    
+    return(outmat)
+}
