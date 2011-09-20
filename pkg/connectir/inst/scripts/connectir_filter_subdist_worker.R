c6a8c5f19fe56de0c087f375f2010ad0bdd397b2
zarrar (Zarrar Shehzad) <zarrar.shehzad@yale.edu>
2011-09-20 20:53:59 +0000

    filter subdist fix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/niftir@207 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/connectir/inst/scripts/connectir_filter_subdist_worker.R
+++ pkg/connectir/inst/scripts/connectir_filter_subdist_worker.R
@@ -110,7 +110,7 @@ tryCatch({
   
   # data dimensions
   vcat(opts$verbose, "...data dimensions")
-  tmp <- attach.big.matrix(file.path(ts.pah$dpath, s.path$dfile))
+  tmp <- attach.big.matrix(file.path(s.path$dpath, s.path$dfile))
   nsubs <- sqrt(nrow(tmp))
   nvoxs <- ncol(tmp)
   rm(tmp); gc(FALSE, TRUE)
